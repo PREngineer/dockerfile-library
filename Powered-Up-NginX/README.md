2f7ed98c029e295b7b233052a0068ec8c55ae0db
Jorge Pabon <pianistapr@hotmail.com>
2024-04-22 17:06:42 -0400

    Reorganizing
    Reorganizing and attempting to fix the ARM64 image that seems to be broken.

--- Powered-Up-NginX/README.md
+++
@@ -1,2 +0,0 @@
-# nginx-powerup-dockerfile
-Dockerfile to build an nginx container that is configurable via web interface.
--- Powered-Up-NginX/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "-> Starting PHP-FPM ... ";
-php-fpm7
-
-echo "-> Starting NginX ... ";
-nginx -g "daemon off;"
